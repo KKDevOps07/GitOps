b77747a9aabfa5bc16fbe4f056d6a7925b2a6429
Kiran <kirangoud983@gmail.com>
2025-07-25 12:02:03 +0400

    second commit

--- providers.tf
+++ providers.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # or the version you want
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.aws_region != "" ? var.aws_region : (env.AWS_DEFAULT_REGION)
 }
